0169714b6ff51d2210eb51ee0666c863b2343914
Rhuam Sena <rhuam.sena@ufrgs.br>
2019-05-17 13:29:33 -0300

    Finish

--- methods.py
+++ methods.py
@@ -26,12 +26,50 @@ def dominant(payoff_matrix, player = 0):
 
 
 def pareto(payoff_matrix):
-    player_sums = list()
 
-    for i in payoff_matrix:
-        for s in i:
-            player_sums.append((s, (sum(s))))
+    mask = np.full(payoff_matrix.shape, False, dtype='bool')
 
-    player_sums.sort(key=lambda tup: tup[1], reverse=True)
+    for line, _ in enumerate(payoff_matrix):
+        for col, sx in enumerate(_):
 
-    print([[s[0] for s in player_sums if s[1] == player_sums[0][1]]])
+            for i in payoff_matrix:
+                for j in i:
+                    mask[line][col] = sx[0] >= j[0] and sx[1] >= j[1]
+
+
+    print(payoff_matrix[mask])
+
+
+def nesh(payoff_matrix):
+    # mask = np.full(payoff_matrix.shape, False, dtype='bool')
+    mask = payoff_matrix.astype('bool, bool')
+    mask.fill(False)
+
+    for line, _ in enumerate(payoff_matrix):
+        for col, sx in enumerate(_):
+            slice_line = list(payoff_matrix[line])
+            slice_col = list(payoff_matrix[:, col])
+
+            mask[line][col][0] = sx[0] >= max(slice_line, key=lambda tup: tup[1])[1]
+            mask[line][col][1] = sx[1] >= max(slice_col, key=lambda tup: tup[0])[0]
+
+    new_mask = np.full(payoff_matrix.shape, False, dtype='bool')
+    for line, _ in enumerate(payoff_matrix):
+        for col, sx in enumerate(_):
+
+            new_mask[line][col] = mask[line][col][0] and mask[line][col][1]
+
+    print(payoff_matrix[new_mask])
+
+
+
+if __name__ == "__main__":
+
+    payoff_prisoners_dilemma = np.array([[(-8, -8), (0, -10)],
+                                        [(-10, 0), (-1, -1)]], dtype='f,f')
+
+    payoff_matrix = np.array([[(5, 0), (5, 4), (0, 3)],
+                              [(5, 5), (0, 3), (5, 2)]], dtype='f,f')
+    # dominant(payoff_prisoners_dilemma, 0)
+
+    pareto(payoff_prisoners_dilemma)
